5069166518f7213d69a0ce93228e459aaa5e085e
fawwaaz muhammad <faww.muh350@gmail.com>
2022-08-15 22:28:33 +0400

    CHANGE

--- price-calculator-kata/Program.cs
+++ price-calculator-kata/Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string Name;
+        double UPC;
+        decimal price;
 
+        Console.WriteLine("Enter Name of Product");
+        Name = Console.ReadLine();
+
+        Console.WriteLine("Enter UPC");
+        UPC = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter Price");
+        price = Decimal.Parse(Console.ReadLine());
+        
+        var priceAfterTax = price+(Decimal.Multiply(price,(decimal)0.20));
+        Console.WriteLine($"Product {Name} reported as ${price} before tax and ${priceAfterTax.ToString("#.##")} after 20% tax.");
+    }
+}
