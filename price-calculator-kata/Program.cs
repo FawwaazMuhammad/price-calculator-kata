6e2234053f375a17f928d5a67f0405910e228939
fawwaaz muhammad <faww.muh350@gmail.com>
2022-08-16 22:53:02 +0400

    #2 Add Discount

--- price-calculator-kata/Program.cs
+++ price-calculator-kata/Program.cs
@@ -5,6 +5,8 @@
         string? Name;
         double UPC;
         decimal price;
+        decimal tax;
+        decimal discount;
 
         Console.WriteLine("Enter Name of Product");
         Name = Console.ReadLine();
@@ -15,18 +17,36 @@
         Console.WriteLine("Enter Price");
         price = Decimal.Parse(Console.ReadLine());
 
-        var priceAfterTax = CalculateTax(price);
-        PrintTaxMessage(Name, price, priceAfterTax);
+        Console.WriteLine("Enter Tax amount  in %");
+        tax = Decimal.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter Discount amount  in %");
+        discount= Decimal.Parse(Console.ReadLine());
+
+        var taxAmount = TaxAmount(price,tax);
+        
+        var priceAfterTax = price + taxAmount;
+        var discountAmount = DiscountAmount(price,discount);
+        var totalPrice = priceAfterTax - discountAmount;
+        PrintTaxMessage(Name,UPC, price, totalPrice,tax,discount,taxAmount,discountAmount);
 
 
     }
-    private static decimal CalculateTax(decimal beforeTaxPrice)
+    private static decimal TaxAmount(decimal beforeTaxPrice, decimal tax)
     {
-        return beforeTaxPrice + (Decimal.Multiply(beforeTaxPrice, (decimal)0.20));
+        return  (Decimal.Multiply(beforeTaxPrice, (tax/100)));
     }
+   
 
-    private static void PrintTaxMessage(string Name, decimal price, decimal priceAfterTax)
+    private static void PrintTaxMessage(string Name,double upc, decimal price, decimal totalPrice,decimal tax,decimal discount,decimal taxAmount,decimal discountAmount)
     {
-        Console.WriteLine($"Product {Name} reported as ${price} before tax and ${priceAfterTax.ToString("#.##")} after 20% tax.");
+        Console.WriteLine($"Sample Product:  {Name}, UPC : {upc} reported as ${price} \n");
+        Console.WriteLine($"Tax: {tax}%,Discount: {discount}");
+        Console.WriteLine($"Tax amount = ${taxAmount.ToString("#.##")}; Discount amount = ${discountAmount.ToString("#.##")}");
+        Console.WriteLine($"Price before = ${price.ToString("#.##")}, price after = ${totalPrice.ToString("#.##")}");
+    }
+    private static decimal DiscountAmount(decimal priceAfterTax, decimal discountAmount)
+    {
+        return  (Decimal.Multiply(priceAfterTax, (discountAmount/100)));
     }
 }
